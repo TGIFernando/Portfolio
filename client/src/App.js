eef4d54ec5d84d26989be1057a1e4069fafa0595
Fernando Chavez <fjchavez01@gmail.com>
2021-01-14 17:58:51 -0800

    started basic design

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from "./components/home"
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <div className="carrot">
+        <h2 className="carrot">^</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/home.jsx
@@ -0,0 +1,17 @@
+function Home(){
+
+
+
+
+    return(
+        <div>
+            <div className="name">
+                <h2 className="hello">Hello, I'm</h2>
+                <h1 className="fern">Fernando Chavez</h1>
+                <h2 className="stack">A Full Stack Web Developer</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home
