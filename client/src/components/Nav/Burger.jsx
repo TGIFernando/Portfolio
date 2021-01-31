d61db9bf41496775e7f18db578fd1fdee60134e7
Fernando Chavez <fjchavez01@gmail.com>
2021-01-30 17:13:26 -0800

    fixed project screen and nav hamburger using mobile first developing

--- client/src/components/Nav/Burger.jsx
+++ client/src/components/Nav/Burger.jsx
@@ -4,37 +4,21 @@ import styled from "styled-components"
 import NavBar from "./NavBar"
 
 const StyledBurger = styled.div`
-    width: 7rem;
-    height: 2rem;
+    height: 1rem;
+    width: 4.5rem;
     position: fixed;
     top: 15px;
     right:2.5rem;
     z-index:1;
-    @media (max-width: 412px){
-        height: 1rem;
-        width: 4.6rem;
-    }
-    @media (max-width: 375px){
-        height: 1rem;
-        width: 4.5rem;
-    }
 
 div{
-    width: 10rem;
-    height: 1rem;
+    width:10rem;
+    height:.5rem;
     background-color: ${({ open }) => open ? "white" : "black"};
     border-radius: 10px;
     transform-origin: 0;
     margin: 5px;
     transition: all 0.3s linear;
-    @media (max-width: 412px){
-    width:10rem;
-    height:.5rem;
-    }
-    @media (max-width: 375px){
-    width:10rem;
-    height:.5rem;
-    }
 
     &:nth-child(1){
         transform: ${({open}) => open ? "rotate(25.5deg)" : "rotate(0)"}
@@ -46,8 +30,7 @@ div{
     &:nth-child(3){
         transform: ${({open}) => open ? "rotate(-25.5deg)" : "rotate(0)"}
     }
-}
-`
+}`
 
 function Burger() {
     const [open, setOpen] = useState(false)
--- client/src/styles/ProjectsStyles.js
+++ client/src/styles/ProjectsStyles.js
@@ -1,26 +1,13 @@
 import styled from "styled-components"
 
 const MainDiv = styled.div`
-    height: 100vh;
     background-color: #5C6D70;
-    @media (max-width: 1920px){
-    height: 150vh;
-    }
-    @media (max-width: 412px){
-    height: 300vh;
-    }
-    @media (max-width: 375px){
-    height: 300vh;
-    }
 `
 const Proj = styled.h1`
     text-align: center;
-    font-size: 8rem;
+    font-size: 5rem;
     color: #F8C7CC;
     padding: 1rem;
-    @media (max-width: 412px){
-    font-size: 6rem;
-    }
 `
 
 const FlexyDiv = styled.div`
